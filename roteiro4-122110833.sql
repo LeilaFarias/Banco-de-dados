85d7a01fc978039673bf13cf3adf578873377363
Leila Farias <leilafarias1324@gmail.com>
2024-08-16 11:38:48 -0300

    inicia roteiro 5

--- roteiro4-122110833.sql
+++ roteiro4-122110833.sql
@@ -1,3 +1,4 @@
+
 1. SELECT dname,dnumber,mgrssn,mgrstartdate FROM department;
 2. SELECT essn,dependent_name,sex,bdate,relationship FROM dependent;
 3. SELECT dnumber,dlocation FROM dept_locations;
@@ -7,9 +8,9 @@
 
 7. SELECT fname,lname FROM employee WHERE sex = 'M';
 8. SELECT fname FROM employee WHERE superssn IS NULL;
-9. SELECT b.fname AS funcionario ,e.fname AS supervisor FROM EMPLOYEE AS e INNER JOIN EMPLOYEE AS b ON e.ssn=b.superssn; -- ERRADO
+9. SELECT b.fname AS funcionario ,e.fname AS supervisor FROM EMPLOYEE AS e INNER JOIN EMPLOYEE AS b ON e.ssn=b.superssn; 
 10. SELECT funcionario FROM (SELECT b.fname AS funcionario ,e.fname AS supervisor FROM EMPLOYEE AS e INNER JOIN EMPLOYEE A
-S b ON e.ssn=b.superssn) AS func_sup WHERE supervisor = 'Franklin'; -- ERRADO
+S b ON e.ssn=b.superssn) AS func_sup WHERE supervisor = 'Franklin'; 
 
-11. SELECT d.dname, l.dlocation FROM department AS d INNER JOIN dept_locations AS l ON d.dnumber=l.dnumber; -- ERRADO
-12. SELECT dname FROM (SELECT d.dname, l.dlocation FROM department AS d INNER JOIN dept_locations AS l ON d.dnumber=l.dnumber) AS depar_locat WHERE dlocation LIKE 'S%'; --ERRADO
+11. SELECT d.dname, l.dlocation FROM department AS d INNER JOIN dept_locations AS l ON d.dnumber=l.dnumber;
+12. SELECT dname FROM (SELECT d.dname, l.dlocation FROM department AS d INNER JOIN dept_locations AS l ON d.dnumber=l.dnumber) AS depar_locat WHERE dlocation LIKE 'S%'; 
